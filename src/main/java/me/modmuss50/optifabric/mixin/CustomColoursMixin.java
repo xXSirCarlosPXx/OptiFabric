33390ea581dc9282e07e5f7563b5dda8298c84e9
Chocohead <Chocohead@Aroma1997.org>
2021-01-24 18:43:51 +0000

    Fix mod foliage always being tinted
    Fixes #121

---
+++ src/main/java/me/modmuss50/optifabric/mixin/CustomColoursMixin.java
@@ -0,0 +1,35 @@
+package me.modmuss50.optifabric.mixin;
+
+import org.objectweb.asm.Opcodes;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Coerce;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.client.color.block.BlockColors;
+import net.minecraft.client.render.model.BakedQuad;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.world.BlockRenderView;
+
+@Pseudo
+@Mixin(targets = "net/optifine/CustomColors", remap = false)
+abstract class CustomColoursMixin {
+	@Inject(method = "getColorMultiplier", cancellable = true,
+			at = @At(value = "FIELD", target = "Lnet/minecraft/block/Blocks;LILY_PAD:Lnet/minecraft/block/Block;", opcode = Opcodes.GETSTATIC, remap = true))
+	private static void skip(BakedQuad quad, BlockState state, BlockRenderView world, BlockPos pos, @Coerce Object renderEnv, CallbackInfoReturnable<Integer> call) {
+		if (!"minecraft".equals(Registry.BLOCK.getId(state.getBlock()).getNamespace()) && getBlockColors().getColor(state, world, pos, quad.getColorIndex()) == -1) {
+			call.setReturnValue(-1); //Avoid tinting a mod block which wouldn't otherwise be tinted
+		}
+	}
+
+	@Shadow
+	public static BlockColors getBlockColors() {
+		throw new AssertionError("Unexpectedly reached code path");
+	}
+}
