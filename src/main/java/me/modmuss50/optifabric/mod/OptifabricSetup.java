127687d21bb6c356e15b2ce62245340376c6d67b
Chocohead <Chocohead@Aroma1997.org>
2021-06-01 03:34:04 +0100

    Fix Dawn 1.8
    Fixes #213

--- src/main/java/me/modmuss50/optifabric/mod/OptifabricSetup.java
+++ src/main/java/me/modmuss50/optifabric/mod/OptifabricSetup.java
@@ -190,7 +190,7 @@ public class OptifabricSetup implements Runnable {
 			Mixins.addConfiguration("optifabric.compat.dawn.older-mixins.json");
 		} else if (isPresent("dawn", ">1.4 <1.5")) {
 			Mixins.addConfiguration("optifabric.compat.dawn.old-mixins.json");
-		} else if (isPresent("dawn", ">=1.5")) {
+		} else if (isPresent("dawn", ">=1.5 <1.8")) {
 			Mixins.addConfiguration("optifabric.compat.dawn.mixins.json");
 		}
 
