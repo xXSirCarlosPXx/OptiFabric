1b90e34455eb6b530051e998e57dea527c2037e3
Chocohead <Chocohead@Aroma1997.org>
2021-11-22 08:00:41 +0000

    Fix options screen
    Missed a changed mapping, would help if Mixin didn't skip warning about injector targets if the target method isn't present
    Fixes #456
    Fixes #457
    Fixes #458
    Fixes #455

--- src/main/java/me/modmuss50/optifabric/mixin/VideoOptionsScreenMixin.java
+++ src/main/java/me/modmuss50/optifabric/mixin/VideoOptionsScreenMixin.java
@@ -16,7 +16,7 @@ import net.fabricmc.loader.api.FabricLoader;
 class VideoOptionsScreenMixin {
 	@Inject(method = "actionPerformed(Lnet/optifine/gui/GuiButtonOF;I)V",
 			slice = @Slice(from = @At(value = "CONSTANT", args = "intValue=231"), to = @At(value = "NEW", target = "net/optifine/shaders/gui/GuiShaders")),
-			at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/screen/VideoOptionsScreen;client:Lnet/minecraft/client/MinecraftClient;", opcode = Opcodes.GETFIELD, remap = true),
+			at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/screen/option/VideoOptionsScreen;client:Lnet/minecraft/client/MinecraftClient;", opcode = Opcodes.GETFIELD, remap = true),
 			cancellable = true, remap = false)
 	private void actionPerformed(CallbackInfo call) {
 		if (FabricLoader.getInstance().isModLoaded("satin")) {
