d6167e4c37ea44036794bc5340d037d2e2ecb406
lordpipe <lordpipe@protonmail.com>
2022-03-14 03:06:11 -0600

    fallbacks for updated mixins to still work on old versions
    this also fixes indentation to use tabs

--- src/main/java/me/modmuss50/optifabric/mixin/CustomColoursMixin.java
+++ src/main/java/me/modmuss50/optifabric/mixin/CustomColoursMixin.java
@@ -7,11 +7,13 @@ import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Coerce;
+import org.spongepowered.asm.mixin.injection.Group;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.client.color.block.BlockColors;
+import net.minecraft.client.render.model.BakedQuad;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.BlockRenderView;
@@ -19,10 +21,20 @@ import net.minecraft.world.BlockRenderView;
 @Pseudo
 @Mixin(targets = "net/optifine/CustomColors", remap = false)
 abstract class CustomColoursMixin {
+	@Group(name = "customColorsMixins", min = 1)
 	@Inject(method = "getColorMultiplier(ZLnet/minecraft/block/BlockState;Lnet/minecraft/world/BlockRenderView;Lnet/minecraft/util/math/BlockPos;Lnet/optifine/render/RenderEnv;)I", cancellable = true, remap = true,
 			at = @At(value = "FIELD", target = "Lnet/minecraft/block/Blocks;LILY_PAD:Lnet/minecraft/block/Block;", opcode = Opcodes.GETSTATIC, remap = true))
 	private static void skip(boolean quadHasTintIndex, BlockState blockState, BlockRenderView blockAccess, BlockPos blockPos, @Coerce Object renderEnv, CallbackInfoReturnable<Integer> call) {
 		if (!"minecraft".equals(Registry.BLOCK.getId(blockState.getBlock()).getNamespace())) {
+			call.setReturnValue(-1); //Avoid tinting a mod block which wouldn't otherwise be tinted
+		}
+	}
+
+	@Group(name = "customColorsMixins", min = 1)
+	@Inject(method = "getColorMultiplier", cancellable = true,
+			at = @At(value = "FIELD", target = "Lnet/minecraft/block/Blocks;LILY_PAD:Lnet/minecraft/block/Block;", opcode = Opcodes.GETSTATIC, remap = true))
+	private static void skipOld(BakedQuad quad, BlockState state, BlockRenderView world, BlockPos pos, @Coerce Object renderEnv, CallbackInfoReturnable<Integer> call) {
+		if (!"minecraft".equals(Registry.BLOCK.getId(state.getBlock()).getNamespace()) && getBlockColors().getColor(state, world, pos, quad.getColorIndex()) == -1) {
 			call.setReturnValue(-1); //Avoid tinting a mod block which wouldn't otherwise be tinted
 		}
 	}
--- src/main/java/me/modmuss50/optifabric/mixin/ParticleManagerMixin.java
+++ src/main/java/me/modmuss50/optifabric/mixin/ParticleManagerMixin.java
@@ -15,9 +15,9 @@ import net.minecraft.world.BlockRenderView;
 @Pseudo
 @Mixin(targets = "net/minecraft/client/particle/ParticleManager")
 public abstract class ParticleManagerMixin {
-    @Inject(method = "updateTerrainParticleColor", at = @At("HEAD"), cancellable = true, remap = true)
-    private static void updateTerrainParticleColor(Particle particle, BlockState state, BlockRenderView world, BlockPos pos, @Coerce Object renderEnv, CallbackInfo call) {
-        // guard this optifine function against calls with null `renderEnv`
-        if (renderEnv == null) call.cancel();
-    }
+	@Inject(method = "updateTerrainParticleColor", at = @At("HEAD"), cancellable = true, remap = true, require = 0)
+	private static void updateTerrainParticleColor(Particle particle, BlockState state, BlockRenderView world, BlockPos pos, @Coerce Object renderEnv, CallbackInfo call) {
+		// guard this optifine function against calls with null `renderEnv`
+		if (renderEnv == null) call.cancel();
+	}
 }
