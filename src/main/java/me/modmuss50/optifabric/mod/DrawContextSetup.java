0bb44d31800b8b8653356d4527fa6af769c64f69
XXMA16 <mihnea.gheorghe16@gmail.com>
2023-07-13 13:14:04 +0300

    Clean up `DrawContextSetup`

--- src/main/java/me/modmuss50/optifabric/mod/DrawContextSetup.java
+++ src/main/java/me/modmuss50/optifabric/mod/DrawContextSetup.java
@@ -2,6 +2,7 @@ package me.modmuss50.optifabric.mod;
 
 import com.chocohead.mm.api.ClassTinkerers;
 import me.modmuss50.optifabric.util.RemappingUtils;
+import net.fabricmc.tinyremapper.IMappingProvider.Member;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.tree.*;
 
@@ -11,8 +12,7 @@ public class DrawContextSetup implements Runnable {
         if (OptifabricSetup.isPresent("minecraft", ">=1.20")) {
             // changes call to DrawContext.drawTextWithShadow(LTextRenderer;String;III)I
             ClassTinkerers.addTransformation("me/modmuss50/optifabric/mod/DrawContext", node -> {
-                String drawContext = "class_332";
-                String drawTextWithShadowDesc = "(Lnet/minecraft/class_327;Ljava/lang/String;III)I";
+                Member drawTextWithShadow = RemappingUtils.mapMethod("class_332", "method_25303", "(Lnet/minecraft/class_327;Ljava/lang/String;III)I");
                 for (MethodNode method : node.methods) {
                     if ("drawTextWithShadow".equals(method.name)) {
                         for (AbstractInsnNode insn : method.instructions) {
@@ -22,17 +22,18 @@ public class DrawContextSetup implements Runnable {
                                 method.instructions.insert(insn.getNext(), new VarInsnNode(Opcodes.ALOAD, 0));
                             // cast to DrawContext instead of MatrixStack
                             } else if (insn.getOpcode() == Opcodes.CHECKCAST) {
-                                ((TypeInsnNode) insn).desc = RemappingUtils.getClassName(drawContext);
+                                ((TypeInsnNode) insn).desc = drawTextWithShadow.owner;
                             // change method
                             } else if (insn.getOpcode() == Opcodes.INVOKEVIRTUAL) {
-                                ((MethodInsnNode) insn).owner = RemappingUtils.getClassName(drawContext);
-                                ((MethodInsnNode) insn).desc = RemappingUtils.mapMethodDescriptor(drawTextWithShadowDesc);
-                                ((MethodInsnNode) insn).name = RemappingUtils.getMethodName(drawContext, "method_25303", drawTextWithShadowDesc);
+                                ((MethodInsnNode) insn).owner = drawTextWithShadow.owner;
+                                ((MethodInsnNode) insn).desc = drawTextWithShadow.desc;
+                                ((MethodInsnNode) insn).name = drawTextWithShadow.name;
                             // method takes ints instead of floats
                             } else if (insn.getOpcode() == Opcodes.I2F) {
                                 method.instructions.remove(insn);
                             }
                         }
+                        break;
                     }
                 }
             });
